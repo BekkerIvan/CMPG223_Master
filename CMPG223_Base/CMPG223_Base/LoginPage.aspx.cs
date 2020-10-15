31e0927602de44215f4db31044292714fe41728a
BekkerIvan <bekkerivan665@gmail.com>
2020-10-16 01:02:26 +0200

    EmergencyService_DONE

--- CMPG223_Base/CMPG223_Base/LoginPage.aspx.cs
+++ CMPG223_Base/CMPG223_Base/LoginPage.aspx.cs
@@ -68,6 +68,11 @@ namespace CMPG223_Base
                     while (sqlDataReader.Read()) {
                         if (txtUsername.Text == sqlDataReader.GetString(3)) {
                                 if (txtPassword.Text == sqlDataReader.GetString(4)) {
+
+                                sqlCommandObj.CommandText
+
+
+
                                 sqlCommandObj.Parameters.Clear();
                                 Session["EMPLOYEE_ID"] = sqlDataReader.GetInt32(0);
                                 sqlCommandObj.CommandText ="INSERT INTO EMPLOYEE_LOG (DATE, LOG_IN_TIME, EMPLOYEE_ID) VALUES ('"+DateTime.Today+"', '"+DateTime.Now+"', '"+ sqlDataReader.GetInt32(0) + "')";
